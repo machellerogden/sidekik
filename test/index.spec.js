0bbed804fec83eb161ec7c913be93b72f7f66bd6
Mac Heller-Ogden <mac@machellerogden.com>
2018-09-30 10:48:43 -0500

    WIP

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,12 +3,51 @@
 const chai = require('chai');
 const { expect } = chai;
 
-const sidekik = require('..');
+const { tokenizer, compiler } = require('..');
 
 describe('sidekik', () => {
-    it('works', () => {
-        const input  = '(foo 123 (bar "baz"))';
-        const output = 'foo(123, bar("baz"));';
-        expect(sidekik.compiler(input)).to.equal(output);
+    describe('tokenizer', () => {
+        it('works', () => {
+            const input  = '(foo 123 (bar "baz"))';
+            const output = [ {
+                type: 'paren',
+                value: '('
+            }, {
+                type: 'ref',
+                value: 'foo'
+            }, {
+                type: 'whitespace'
+            }, {
+                type: 'number',
+                value: '123'
+            }, {
+                type: 'whitespace'
+            }, {
+                type: 'paren',
+                value: '('
+            }, {
+                type: 'ref',
+                value: 'bar'
+            }, {
+                type: 'whitespace'
+            }, {
+                type: 'string',
+                value: 'baz'
+            }, {
+                type: 'paren',
+                value: ')'
+            }, {
+                type: 'paren',
+                value: ')'
+            } ];
+            expect(tokenizer(input)).to.eql(output);
+        });
+    });
+    describe('compiler', () => {
+        it('works', () => {
+            const input  = '(foo 123 (bar "baz"))';
+            const output = 'foo(123, bar("baz"));';
+            expect(compiler(input)).to.equal(output);
+        });
     });
 });
