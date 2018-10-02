00a5215f9b1be204b7a02992ea3a3f956468b9e9
Mac Heller-Ogden <mac@machellerogden.com>
2018-10-02 15:53:10 -0500

    WIP

--- compile.js
+++ compile.js
@@ -6,4 +6,7 @@ const pull = require('pull-stream');
 const tokenizer = require('./lib/tokenizer');
 const parser = require('./lib/parser');
 
-pull(tokenizer(), parser(), pull.drain(console.log));
+const { inspect } = require('util');
+const log = pull.drain(v => console.log(inspect(v, { depth: null, colors: true })));
+
+pull(tokenizer(), parser(), log);
